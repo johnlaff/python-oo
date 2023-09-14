e54c70df78c34a3c4ea8dd06d5e2b55563449ba1
johnlaff <joaoaraxaiba@gmail.com>
2023-09-14 12:59:43 -0300

    Verificação de limite e adição de métodos estáticos
    Este commit adiciona o método "__pode_sacar" à Classe Conta. Este método
    verifica se um saque pode ser realizado com base no saldo e limite
    disponível na conta. Caso o valor do saque seja maior do que o saldo
    somado ao limite, é exibida uma mensagem de erro informando que o valor é
    maior do que o limite disponível.
    
    Além disso, este commit também inclui dois novos métodos estáticos na
    Classe Conta: "codigo_banco" que retorna o código do banco padrão como
    "001", e "codigos_bancos" que retorna um dicionário com os códigos de
    diferentes bancos.

--- conta.py
+++ conta.py
@@ -14,8 +14,15 @@ class Conta:
     def deposita(self, valor):
         self.__saldo += valor
 
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
+
     def saca(self, valor):
-        self.__saldo -= valor
+        if self.__pode_sacar(valor):
+            self.__saldo -= valor
+        else:
+            print(f"O valor de {valor} é maior do que o limite disponível.")
 
     def transfere(self, valor, destino):
         self.saca(valor)
@@ -36,3 +43,11 @@ class Conta:
     @limite.setter
     def limite(self, limite):
         self.__limite = limite
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco': '237'}
